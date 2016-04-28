77d69fe65b3c44c16bee8a0edd0fca664f15aa7f
Beth Richardson <beth.richardson@blackbaud.com>
2016-04-28 10:10:01 -0500

    Initialize polymer project

---
+++ polymer/test/client/app/app.constant.js
@@ -0,0 +1,9 @@
+(function(angular, undefined) {
+'use strict';
+
+angular.module('controlComplexApp.constants', [])
+
+.constant('appConfig', {userRoles:['guest','user','admin']})
+
+;
+})(angular);
---
+++ polymer/test/client/components/footer/footer.directive.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('controlComplexApp')
+  .directive('footer', function() {
+    return {
+      templateUrl: 'components/footer/footer.html',
+      restrict: 'E',
+      link: function(scope, element) {
+        element.addClass('footer');
+      }
+    };
+  });
