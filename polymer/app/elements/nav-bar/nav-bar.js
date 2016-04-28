4b32a16dab977920b7f11526fcec42e1c87f96d8
Robert Tester <robert.tester@blackbaud.com>
2016-04-28 11:25:36 -0500

    robert example

---
+++ polymer/app/elements/nav-bar/nav-bar.js
@@ -0,0 +1,26 @@
+class NavBar {
+
+  get is() {
+    return "nav-bar";
+  }
+
+  beforeRegister() {
+    this.properties = {
+      foo: {
+        type: String,
+        value: 'blah',
+        notify: true
+      }
+    };
+
+    this.listeners = {
+      'click': 'click'
+    }
+  }
+
+  click() {
+    alert('you clicked me');
+  }
+}
+
+Polymer(NavBar);
