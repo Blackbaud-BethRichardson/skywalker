cfeb8736f96404dafe9dad202716bd49a528345c
Robert Tester <robert.tester@blackbaud.com>
2016-04-28 12:02:48 -0500

    another commit

--- polymer/app/elements/nav-bar/nav-bar.js
+++ polymer/app/elements/nav-bar/nav-bar.js
@@ -1,26 +1,26 @@
 class NavBar {
 
-  get is() {
-    return "nav-bar";
-  }
-
-  beforeRegister() {
-    this.properties = {
-      foo: {
-        type: String,
-        value: 'blah',
-        notify: true
-      }
-    };
-
-    this.listeners = {
-      'click': 'click'
+    get is() {
+        return "nav-bar";
     }
-  }
 
-  click() {
-    alert('you clicked me');
-  }
+    beforeRegister() {
+        this.properties = {
+            foo: {
+                type: String,
+                value: 'blah',
+                notify: true
+            }
+        };
+        //
+        // this.listeners = {
+        //     'click': 'click'
+        // }
+    }
+    //
+    // click() {
+    //     alert('you clicked me');
+    // }
 }
 
 Polymer(NavBar);
